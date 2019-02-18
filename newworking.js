cabd13ba8dfc4465deae710ca0acaa4a43a1b92a
mkhans <37116170+mkhans@users.noreply.github.com>
2019-02-17 18:49:03 -0700

    Create newworking.js

---
+++ newworking.js
@@ -0,0 +1,24 @@
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1CpOU9TKZXwGu4h40TuGeVHI4fw8eMX_bube2AJY4taY&output=html&usp=sharing');
+    query.setQuery('SELECT A, B label A "A", B "B"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
