122745f87f4c1f39e7a51133e6ed637e96ab1910
mkhans <37116170+mkhans@users.noreply.github.com>
2020-01-23 13:54:38 -0700

    Update ww2.js

--- ww2.js
+++ ww2.js
@@ -84,7 +84,6 @@ $.get("https://api.sunrise-sunset.org/json?lat=40.789900&lng=-111.979100&date=to
 // DATA VIA GAE PYTHON FTP
 // $.get("https://wind-aloft-forecast.appspot.com", function(waloftFcDataGAE) {
 $.get("https://us-central1-wasatchwind.cloudfunctions.net/wind-aloft", function(waloftFcDataGAE) {
-    console.log(waloftFcDataGAE);
     document.getElementById('wind-aloft-fc-start').innerHTML = waloftFcDataGAE.HEADER.START_TIME;
     document.getElementById('wind-aloft-fc-end').innerHTML = waloftFcDataGAE.HEADER.END_TIME;
     document.getElementById('wind-aloft-fc-day').innerHTML = waloftFcDataGAE.HEADER.FORECAST_DAY;
